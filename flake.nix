c581208cee77259a77e216510b5a66823f80320e
Hermann Pauly <hp@campudus.com>
2023-02-07 12:31:42 +0100

    Make importable

--- flake.nix
+++ flake.nix
@@ -12,56 +12,53 @@
         root-env-var = "$PG_ROOT";
         dev-db = "dev";
         test-db = "test";
-        db-path = "${root-env-var}/PG15_DATA";
+        db-path = "${root-env-var}/PG_DATA";
         db-port = "5432";
+        check-env = (pkgs.writeShellScriptBin "check-env" ''
+          if [ -z "${root-env-var}" ]; then
+            echo '${root-env-var}' is not set, can not init database;
+            exit 1;
+          else
+            echo PGHOST=${db-path}
+            echo PGPORT=${db-port}
+          fi
+        '');
       in {
-        devShell = with pkgs;
-          mkShell {
-            buildInputs = [
-              postgresql_15
-              (writeShellScriptBin "check-env" ''
-                if [ -z "${root-env-var}" ]; then
-                  echo '${root-env-var}' is not set, can not init database;
-                  exit 1;
-                else
-                  echo PGHOST=${db-path}
-                  echo PGPORT=${db-port}
-                fi
-              '')
+        devShell = with pkgs; mkShell { };
 
-              (writeShellScriptBin "start-database" ''
-                set -e
-                check-env
-                if [[ ! -d "${db-path}" ]]; then
-                  set -e
-                  initdb -D "${db-path}"
-                  pg_ctl -D ${db-path} -o "-k ${db-path}" -o "-p ${db-port}" -l ${db-path}/database.log start
-                  for DBNAME in {${dev-db},${test-db},$USER}; do
-                    createdb -h ${db-path} -p ${db-port}  $DBNAME && echo Created db $DBNAME
-                  done
-                  psql --host ${db-path} --port ${db-port} \
-                    -tc "ALTER USER $USER WITH SUPERUSER" \
-                    && echo Changed user $USER to superuser
-                elif [[ ! -f "${db-path}/.s.PGSQL.${db-port}.lock" ]]; then
-                  pg_ctl -D ${db-path} -o "-k ${db-path}" -o "-p ${db-port}" -l ${db-path}/database.log start
-                else
-                  echo Postgres is already running for this project
-                fi
-              '')
-              (writeShellScriptBin "stop-database" ''
-                set -e
-                check-env
-                pg_ctl -D ${db-path} stop || rm "${db-path}/.s.PGSQL.${db-port}.lock"
-              '')
-              (writeShellScriptBin "psql-wrapped" ''
-                set -e
-                check-env
-                if [[ -z $PGDATABASE ]]; then export PGDATABASE="${dev-db}"; fi
-                PGHOST="${db-path}" PGPORT="${db-port}" psql "$@"
-              '')
-            ];
-          };
-        PGHOST = "${db-path}";
-        PGPORT = "${db-port}";
+        packages = with pkgs; {
+          start-database = (writeShellScriptBin "start-database" ''
+            set -e
+            ${check-env}/bin/check-env
+            if [[ ! -d "${db-path}" ]]; then
+              set -e
+              initdb -D "${db-path}"
+              pg_ctl -D ${db-path} -o "-k ${db-path}" -o "-p ${db-port}" -l ${db-path}/database.log start
+              for DBNAME in {${dev-db},${test-db},$USER}; do
+                createdb -h ${db-path} -p ${db-port}  $DBNAME && echo Created db $DBNAME
+              done
+              psql --host ${db-path} --port ${db-port} \
+                -tc "ALTER USER $USER WITH SUPERUSER" \
+                && echo Changed user $USER to superuser
+            elif [[ ! -f "${db-path}/.s.PGSQL.${db-port}.lock" ]]; then
+              pg_ctl -D ${db-path} -o "-k ${db-path}" -o "-p ${db-port}" -l ${db-path}/database.log start
+            else
+              echo Postgres is already running for this project
+            fi
+          '');
+          stop-database = (writeShellScriptBin "stop-database" ''
+            set -e
+            ${check-env}/bin/check-env
+            pg_ctl -D ${db-path} stop || rm "${db-path}/.s.PGSQL.${db-port}.lock"
+          '');
+          psql-wrapped = (writeShellScriptBin "psql-wrapped" ''
+            set -e
+            ${check-env}/bin/check-env
+            if [[ -z $PGDATABASE ]]; then export PGDATABASE="${dev-db}"; fi
+            PGHOST="${db-path}" PGPORT="${db-port}" psql "$@"
+          '');
+        };
+
+        defaultPackage = self.packages.${system}.psql-wrapped;
       });
 }
